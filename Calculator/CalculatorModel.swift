c11628df1a4903f6159a6db0907224f541790e5e
Menat Pinesh I <n01205889@nl-00238-016.mediastudies.ca>
2018-04-16 18:53:47 -0400

    UI Button actions added

---
+++ Calculator/CalculatorModel.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by Menat Pinesh I. on 4/16/18.
+//  Copyright © 2018 Ammar Al-Qaraghuli. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    
+    Double output = 0.0;
+    
+    
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputLabel: UILabel!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func zeroBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "0";
+    }
+    
+    @IBAction func oneBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "1";
+    }
+    
+    @IBAction func twoBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "2";
+    }
+    
+    @IBAction func threeBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "3";
+    }
+    
+    @IBAction func fourBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "4";
+    }
+    @IBAction func fiveBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "5";
+    }
+    
+    @IBAction func sixBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "6";
+    }
 
+    @IBAction func sevenBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "7";
+    }
+    
+    @IBAction func eightBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "8";
+    }
+    
+    @IBAction func nineBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + "9";
+    }
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        outputLabel.text = "0";
+    }
+    
+    @IBAction func negateBtn(_ sender: Any) {
+        outputLabel.text = "-" + outputLabel.text!;
+    }
+    
+    @IBAction func percentBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func divideBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func multiplyBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func subtractBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func additionBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func equalBtn(_ sender: Any) {
+    }
+
+    @IBAction func decimalBtn(_ sender: Any) {
+        outputLabel.text = outputLabel.text! + ".";
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
