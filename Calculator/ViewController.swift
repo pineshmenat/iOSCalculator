a11c3191b4cc2d53e7ef9acc08dd4d58902f18a8
Menat Pinesh I <n01205889@nl-00238-016.mediastudies.ca>
2018-04-16 17:45:29 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ammar Al-Qaraghuli on 2018-04-12.
+//  Copyright © 2018 Ammar Al-Qaraghuli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
