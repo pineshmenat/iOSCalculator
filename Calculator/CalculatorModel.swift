1e729c8d20dcaa692fa661e0b6db271c68185a4a
Menat Pinesh I <n01205889@nl-00205-017.local>
2018-04-16 21:25:03 -0400

    So far nothing worth to commit

--- Calculator/CalculatorModel.swift
+++ Calculator/CalculatorModel.swift
@@ -9,8 +9,76 @@
 import Foundation
 
 class Calculator {
+    func Calculator() {
     
-    Double output = 0.0;
+    }
+    var output:Double = 0.0
+    var operation1:Character = " "
+    var operation2:Character = " "
+    var decimal:Bool = false
+    var negate:Bool = false
+    var num1:Double = 0.0
+    var num2:Double = 0.0
+    
+    func calculate(operation:Character) -> Double {
+        switch operation {
+        case "+":
+            
+            break;
+        case "-":
+            
+            break
+        case "*":
+            
+            break
+        case "/":
+            
+            break
+        case "%":
+            
+            break
+        default:
+            break
+        }
+        return output
+    }
+    
+    func numberTapped(number:Character) -> Double{
+        if(operation1 == " ") {
+            return appendNum1(number: number)
+        }else {
+            return appendNum2(number: number)
+        }
+    }
+    
+    func appendNum1(number: Character) -> Double {
+        num1 = num1*10 + Double(String(number))!
+        return num1
+    }
+    
+    func appendNum2(number: Character) -> Double {
+        num2 = num2*10 + Double(String(number))!
+        return num2
+    }
+    
+    func operationTapped(operation:Character) -> Bool {
+        if(operation1 == " ") {
+            operation1 = operation
+            return true
+        }else {
+            if(num2 == 0.0) {
+                operation1 = operation
+            }else {
+                calculate(operation: operation1)
+            }
+            return false
+        }
+    }
+    
+    
+    
+    
+    
     
     
 }
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,74 +13,78 @@ class ViewController: UIViewController {
     @IBOutlet weak var inputLabel: UILabel!
     @IBOutlet weak var outputLabel: UILabel!
     
+    var calculator = Calculator()
+    
+    
     @IBAction func zeroBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "0";
+        outputLabel.text = String(calculator.numberTapped(number: "0"));
     }
     
     @IBAction func oneBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "1";
+        outputLabel.text = String(calculator.numberTapped(number: "1"));
     }
     
     @IBAction func twoBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "2";
+        outputLabel.text = String(calculator.numberTapped(number: "2"));
     }
     
     @IBAction func threeBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "3";
+        outputLabel.text = String(calculator.numberTapped(number: "3"));
     }
     
     @IBAction func fourBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "4";
+        outputLabel.text = String(calculator.numberTapped(number: "4"));
     }
     @IBAction func fiveBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "5";
+        outputLabel.text = String(calculator.numberTapped(number: "5"));
     }
     
     @IBAction func sixBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "6";
+        outputLabel.text = String(calculator.numberTapped(number: "6"));
     }
 
     @IBAction func sevenBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "7";
+        outputLabel.text = String(calculator.numberTapped(number: "7"));
     }
     
     @IBAction func eightBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "8";
+        outputLabel.text = String(calculator.numberTapped(number: "8"));
     }
     
     @IBAction func nineBtn(_ sender: Any) {
-        outputLabel.text = outputLabel.text! + "9";
+        outputLabel.text = String(calculator.numberTapped(number: "9"));
     }
     
     @IBAction func cancelBtn(_ sender: Any) {
-        outputLabel.text = "0";
+        
     }
     
     @IBAction func negateBtn(_ sender: Any) {
-        outputLabel.text = "-" + outputLabel.text!;
+        outputLabel.text = String(calculator.numberTapped(number: "-"));
     }
     
     @IBAction func percentBtn(_ sender: Any) {
-        
+        calculator.operationTapped(operation: "%")
     }
     
     @IBAction func divideBtn(_ sender: Any) {
-        
+        calculator.operationTapped(operation: "%")
     }
     
     @IBAction func multiplyBtn(_ sender: Any) {
-        
+        calculator.operationTapped(operation: "*")
     }
     
     @IBAction func subtractBtn(_ sender: Any) {
-        
+        calculator.operationTapped(operation: "-")
     }
     
     @IBAction func additionBtn(_ sender: Any) {
-        
+        calculator.operationTapped(operation: "+")
     }
     
     @IBAction func equalBtn(_ sender: Any) {
+        calculator.operationTapped(operation: "=")
     }
 
     @IBAction func decimalBtn(_ sender: Any) {
